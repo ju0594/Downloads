0bf4f7b416d6f1205167f8772093e1fd549e7071
HackingGate <i@hackinggate.com>
2021-02-13 23:47:04 +0900

    Fix sha256sum generate

--- generate-page.sh
+++ generate-page.sh
@@ -7,7 +7,7 @@ git config --add remote.origin.fetch '+refs/heads/*:refs/remotes/origin/*'
 git fetch origin gh-pages
 git checkout gh-pages
 # OpenWrt
-sha256sum -b */*.+(bin|img|json|manifest) > sha256sums.txt
+find -regextype posix-extended -regex '.*\.(bin|img|json|manifest)' -exec sha256sum -b {} > sha256sums.txt \;
 tree -H '.' -h --noreport --charset utf-8 -T ${GITHUB_REPOSITORY} -o index.html
 cat github-corner.html >> index.html
 sed -i "s~https://your-url~${GITHUB_SERVER_URL}/${GITHUB_REPOSITORY}~g" index.html
