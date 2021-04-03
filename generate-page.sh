8f4822b2da51bc02fc9dc6960c2997c8e1332973
HackingGate <i@hackinggate.com>
2021-04-03 11:41:54 +0900

    Force build on default branch push (#2)
    * Limit on.push branches to trigger GitHub workflow
    
    * Try override built files
    
    * Limit on.pull_request branches to trigger GitHub workflow
    
    * Fix generate-page.sh file missing
    
    * Add ${{ }}
    
    * Add tree before deploy
    
    * Apply same changes to other workflows
    
    * Fix missing wr703n-openwrt-custom-build.sh

--- generate-page.sh
+++ generate-page.sh
@@ -3,9 +3,6 @@
 # Exit immediately if a simple command exits with a non-zero status
 set -e
 
-git config --add remote.origin.fetch '+refs/heads/*:refs/remotes/origin/*'
-git fetch origin gh-pages
-git checkout gh-pages
 # OpenWrt
 find -regextype posix-extended -regex '.*\.(bin|img|json|manifest)' -exec sha256sum -b {} > sha256sums.txt \;
 tree -H '.' -h --noreport --charset utf-8 -T ${GITHUB_REPOSITORY} -o index.html
