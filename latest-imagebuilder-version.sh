09f68278ce6893c501263c2f5b1ade578a485bb7
HackingGate <i@hackinggate.com>
2021-04-21 01:17:30 +0900

    Support 21.02 (#4)

---
+++ latest-imagebuilder-version.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Exit immediately if a simple command exits with a non-zero status
+set -e
+
+OPENWRT_VERSION=`git ls-remote --tags https://github.com/openwrt/openwrt | grep -E -o '[0-9]+\.[0-9]+\.[0-9]+($|-rc[0-9]+$)' | tail -1`
+IMAGEBUILDER_HTTP_CODE=`curl -s -o /dev/null --head -w "%{http_code}" https://downloads.openwrt.org/releases/${OPENWRT_VERSION}/targets/${1}/${2}/openwrt-imagebuilder-${OPENWRT_VERSION}-${1}-${2}.Linux-x86_64.tar.xz`
+if [[ "$IMAGEBUILDER_HTTP_CODE" != 200 ]]; then
+    # Use previous version if imagebuilder not yet available
+    OPENWRT_VERSION=`git ls-remote --tags https://github.com/openwrt/openwrt | grep -E -o '[0-9]+\.[0-9]+\.[0-9]+($|-rc[0-9]+$)' | tail -2 | head -1`
+fi
+
+echo ${OPENWRT_VERSION}
--- wr703n-openwrt-imagebuilder-build.sh
+++ wr703n-openwrt-imagebuilder-build.sh
@@ -1,35 +1,44 @@
-#!/bin/sh
+#!/bin/bash
 
 # Exit immediately if a simple command exits with a non-zero status
 set -e
 
+OPENWRT_MAJOR_VERSION=`echo ${OPENWRT_VERSION} | grep -E -o '[0-9]+\.[0-9]+'`
+TARGET='ath79'
+DEVICE_NAME='tl-wr703n'
+
+# Workaround to fix ath79 missing wifi
+if [[ $OPENWRT_MAJOR_VERSION < '20.02' ]]; then
+    TARGET='ar71xx'
+    DEVICE_NAME='tl-wr703n-v1'
+fi
+
 echo "Download OpenWrt Image Builder ${OPENWRT_VERSION}"
 
 # Download imagebuilder for WR703N.
-aria2c -c -x4 -s4 https://downloads.openwrt.org/releases/${OPENWRT_VERSION}/targets/ath79/tiny/openwrt-imagebuilder-${OPENWRT_VERSION}-ath79-tiny.Linux-x86_64.tar.xz
+aria2c -c -x4 -s4 https://downloads.openwrt.org/releases/${OPENWRT_VERSION}/targets/${TARGET}/tiny/openwrt-imagebuilder-${OPENWRT_VERSION}-${TARGET}-tiny.Linux-x86_64.tar.xz
 
 # Extract & remove used file & cd to the directory
-tar -xvf openwrt-imagebuilder-${OPENWRT_VERSION}-ath79-tiny.Linux-x86_64.tar.xz
-rm openwrt-imagebuilder-${OPENWRT_VERSION}-ath79-tiny.Linux-x86_64.tar.xz
-cd openwrt-imagebuilder-${OPENWRT_VERSION}-ath79-tiny.Linux-x86_64/
+tar -xvf openwrt-imagebuilder-${OPENWRT_VERSION}-${TARGET}-tiny.Linux-x86_64.tar.xz
+rm openwrt-imagebuilder-${OPENWRT_VERSION}-${TARGET}-tiny.Linux-x86_64.tar.xz
+cd openwrt-imagebuilder-${OPENWRT_VERSION}-${TARGET}-tiny.Linux-x86_64/
 
 # Replace tiny-tp-link.mk
-OPENWRT_MAJOR_VERSION=`echo ${OPENWRT_VERSION} | grep -E -o '[0-9]+\.[0-9]+'`
-cd target/linux/ath79/image
-wget https://github.com/HackingGate/openwrt/raw/openwrt-${OPENWRT_MAJOR_VERSION}-modified-device/target/linux/ath79/image/tiny-tp-link.mk -O tiny-tp-link.mk
+cd target/linux/${TARGET}/image
+wget https://github.com/HackingGate/openwrt/raw/openwrt-${OPENWRT_MAJOR_VERSION}-modified-device/target/linux/${TARGET}/image/tiny-tp-link.mk -O tiny-tp-link.mk
 cd -
 
 # Use https
 sed -i 's/http:/https:/g' .config repositories.conf
 
 # Make all kernel modules built-in
-sed -i -e "s/=m/=y/g" build_dir/target-mips_24kc_musl/linux-ath79_tiny/linux-*/.config
+sed -i -e "s/=m/=y/g" build_dir/target-mips_24kc_musl/linux-${TARGET}_tiny/linux-*/.config
 
 # Run the final build configuration
-make image PROFILE=tplink_tl-wr703n \
+make image PROFILE=${DEVICE_NAME} \
 PACKAGES="luci"
 
 # Result
-cd bin/targets/ath79/tiny/
+cd bin/targets/${TARGET}/tiny/
 cat profiles.json | jq
 cat sha256sums
