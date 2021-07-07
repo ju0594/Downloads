4b4f1dec148649781b67d68b84de2504042ea4f4
HackingGate <i@hackinggate.com>
2021-07-07 21:50:27 +0900

    Change stubby to https-dns-proxy

--- r7800-openwrt-imagebuilder-build.sh
+++ r7800-openwrt-imagebuilder-build.sh
@@ -22,8 +22,9 @@ sed -i -e "s/=m/=y/g" build_dir/target-arm_cortex-a15+neon-vfpv4_musl_eabi/linux
 # Run the final build configuration
 make image PROFILE=netgear_r7800 \
 PACKAGES="luci ca-bundle ca-certificates libustream-wolfssl \
+https-dns-proxy luci-app-https-dns-proxy \
 -wpad-mini -wpad-basic -wpad-basic-wolfssl wpad-wolfssl usbutils block-mount e2fsprogs samba4-server luci-app-samba4 \
-aria2 luci-app-aria2 ariang stubby curl wget tcpdump kmod-fs-ext4 kmod-usb-storage kmod-usb-storage-uas \
+aria2 luci-app-aria2 ariang curl wget tcpdump kmod-fs-ext4 kmod-usb-storage kmod-usb-storage-uas \
 luci-app-statistics collectd-mod-cpu collectd-mod-interface collectd-mod-memory collectd-mod-ping collectd-mod-rrdtool collectd-mod-wireless \
 -dnsmasq dnsmasq-full map mwan3 luci-app-mwan3 6in4 luci-proto-hnet \
 luci-app-wireguard luci-proto-wireguard adblock luci-app-adblock avahi-utils netatalk \
