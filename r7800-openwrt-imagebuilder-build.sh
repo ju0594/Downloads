3dec715e2c2b2f200444d305afda75bc01b432db
HackingGate <i@hackinggate.com>
2021-08-10 22:26:11 +0900

    Remove avahi-utils netatalk

--- r7800-openwrt-imagebuilder-build.sh
+++ r7800-openwrt-imagebuilder-build.sh
@@ -27,7 +27,7 @@ https-dns-proxy luci-app-https-dns-proxy \
 aria2 luci-app-aria2 ariang curl wget kmod-fs-ext4 kmod-usb-storage kmod-usb-storage-uas \
 luci-app-statistics collectd-mod-cpu collectd-mod-interface collectd-mod-memory collectd-mod-ping collectd-mod-rrdtool collectd-mod-wireless \
 -dnsmasq dnsmasq-full map mwan3 luci-app-mwan3 6in4 luci-proto-hnet \
-luci-app-wireguard luci-proto-wireguard adblock luci-app-adblock tcpdump-mini avahi-utils netatalk \
+luci-app-wireguard luci-proto-wireguard adblock luci-app-adblock tcpdump-mini \
 diffutils git"
 
 # Result
