304a4e9da175adbe28c2e41731b0af1acc3cc13e
HackingGate <i@hackinggate.com>
2021-09-12 23:24:05 +0900

    Fetch lastest tag (fix sort)

--- latest-imagebuilder-version.sh
+++ latest-imagebuilder-version.sh
@@ -3,7 +3,7 @@
 # Exit immediately if a simple command exits with a non-zero status
 set -e
 
-OPENWRT_VERSION=`git ls-remote --tags https://github.com/openwrt/openwrt | grep -E -o '[0-9]+\.[0-9]+\.[0-9]+($|-rc[0-9]+$)' | tail -1`
+OPENWRT_VERSION=`curl -s https://api.github.com/repos/openwrt/openwrt/tags | jq -r '.[0]["name"]' | grep -E -o '[0-9]+\.[0-9]+\.[0-9]+($|-rc[0-9]+$)'`
 IMAGEBUILDER_HTTP_CODE=`curl -s -o /dev/null --head -w "%{http_code}" https://downloads.openwrt.org/releases/${OPENWRT_VERSION}/targets/${1}/${2}/openwrt-imagebuilder-${OPENWRT_VERSION}-${1}-${2}.Linux-x86_64.tar.xz`
 if [[ "$IMAGEBUILDER_HTTP_CODE" != 200 ]]; then
     # Use previous version if imagebuilder not yet available
