710957ef5203a84ef883d7ff811da23a608174a6
HackingGate <i@hackinggate.com>
2021-09-12 23:41:42 +0900

    Fix previous version

--- latest-imagebuilder-version.sh
+++ latest-imagebuilder-version.sh
@@ -6,8 +6,10 @@ set -e
 OPENWRT_VERSION=`curl -s https://api.github.com/repos/openwrt/openwrt/tags | jq -r '.[0]["name"]' | grep -E -o '[0-9]+\.[0-9]+\.[0-9]+($|-rc[0-9]+$)'`
 IMAGEBUILDER_HTTP_CODE=`curl -s -o /dev/null --head -w "%{http_code}" https://downloads.openwrt.org/releases/${OPENWRT_VERSION}/targets/${1}/${2}/openwrt-imagebuilder-${OPENWRT_VERSION}-${1}-${2}.Linux-x86_64.tar.xz`
 if [[ "$IMAGEBUILDER_HTTP_CODE" != 200 ]]; then
+    echo "Not found for https://downloads.openwrt.org/releases/${OPENWRT_VERSION}/targets/${1}/${2}/openwrt-imagebuilder-${OPENWRT_VERSION}-${1}-${2}.Linux-x86_64.tar.xz"
     # Use previous version if imagebuilder not yet available
-    OPENWRT_VERSION=`git ls-remote --tags https://github.com/openwrt/openwrt | grep -E -o '[0-9]+\.[0-9]+\.[0-9]+($|-rc[0-9]+$)' | tail -2 | head -1`
+    OPENWRT_VERSION=`curl -s https://api.github.com/repos/openwrt/openwrt/tags | jq -r '.[1]["name"]' | grep -E -o '[0-9]+\.[0-9]+\.[0-9]+($|-rc[0-9]+$)'`
+    echo "Use previous version ${OPENWRT_VERSION} instead"
 fi
 
 echo ${OPENWRT_VERSION}
